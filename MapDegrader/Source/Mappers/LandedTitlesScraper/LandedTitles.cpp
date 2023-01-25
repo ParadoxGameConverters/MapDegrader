628bf9a4722a9aeb875853c85fcced997379da19
Ivo Elezovic <ivo.elezovic@gmail.com>
2023-01-25 10:43:22 +0100

    clang

--- MapDegrader/Source/Mappers/LandedTitlesScraper/LandedTitles.cpp
+++ MapDegrader/Source/Mappers/LandedTitlesScraper/LandedTitles.cpp
@@ -1,7 +1,7 @@
 #include "LandedTitles.h"
+#include "CommonRegexes.h"
 #include "Log.h"
 #include "ParserHelpers.h"
-#include "CommonRegexes.h"
 
 void LandedTitles::loadTitles(std::istream& theStream, const std::string& theDJLiege, const std::string& theName)
 {
@@ -28,19 +28,23 @@ void LandedTitles::loadTitles(const std::string& fileName)
 
 void LandedTitles::registerKeys()
 {
-	registerRegex(R"((e|k|d|c|b)_[A-Za-z0-9_\-\']+)", [this](const std::string& titleName, std::istream& theStream) {
-		LandedTitles newTitle;
-		newTitle.loadTitles(theStream, name, titleName);
+	registerRegex(R"((e|k|d|c|b)_[A-Za-z0-9_\-\']+)",
+		 [this](const std::string& titleName, std::istream& theStream)
+		 {
+			 LandedTitles newTitle;
+			 newTitle.loadTitles(theStream, name, titleName);
 
-		// Store ourselves
-		titles.insert(std::pair(titleName, newTitle));
+			 // Store ourselves
+			 titles.insert(std::pair(titleName, newTitle));
 
-		// Pull the titles beneath this one and add them to the lot.
-		const auto& locatedTitles = newTitle.getTitles();
-		titles.insert(locatedTitles.begin(), locatedTitles.end());
-	});
-	registerKeyword("province", [this](const std::string& unused, std::istream& theStream) {
-		provinceID = commonItems::singleInt(theStream).getInt();
-	});
+			 // Pull the titles beneath this one and add them to the lot.
+			 const auto& locatedTitles = newTitle.getTitles();
+			 titles.insert(locatedTitles.begin(), locatedTitles.end());
+		 });
+	registerKeyword("province",
+		 [this](const std::string& unused, std::istream& theStream)
+		 {
+			 provinceID = commonItems::singleInt(theStream).getInt();
+		 });
 	registerRegex(commonItems::catchallRegex, commonItems::ignoreItem);
 }
